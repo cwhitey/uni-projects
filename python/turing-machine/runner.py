02d3e4e0ed5d20906534b7790cccc762686e7519
Callum White <callumw1991@gmail.com>
2015-04-28 16:32:32 +1000

    Added turing machine runner

---
+++ python/turing-machine/runner.py
@@ -0,0 +1,7 @@
+'''This is an example driver for the turingmachine.py module.  You do
+not have to supply this code.'''
+
+from turingmachine import run_turing
+
+if __name__ == "__main__":
+    run_turing("flipper.xml")
